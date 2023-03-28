15e63633c381c35531c15eeb45b94fd151b1f305
vassaneer <pearitono@gmail.com>
2023-03-28 11:20:57 +0700

    change method

--- src/name_get.php
+++ src/name_get.php
@@ -12,12 +12,13 @@
 <form action="../index.html">
  <div class ='bubble'>
  <?php 
-	if (strcmp($_POST["name"], "พี่เป้")==0){ 
+        $name = $_GET["name"];
+	if (strcmp($name, "พี่เป้")==0){ 
               $str = ["เป็นคนดีย์", "คนเก่ง", "คนซื่อสัตย์", "คนขี้เกรงใจ", "คนรู้ทัน"];
                foreach($str as $s){
 		 echo "<label><div class ='bubble'><div class='bubble-content'> ".$s."</div> </div></label>";
 		}
-	}else if (strcmp($_POST["name"], "น้องเจ")==0){ 
+	}else if (strcmp($name, "น้องเจ")==0){ 
               $str = ["เป็นคนซุ่มซ่าม เอ่ย คนโก๊ะๆ","จี้เก่ง เซ็นท์ดีจริงๆ", "แม่สาวไอติม ดีนะ ไม่ใด้ให้ยาคูล", "เป็นเศรษฐีแน่ๆ เปย์ข้าวหนุ่มๆเก่ง","มีหนุ่มๆจะไปหา ก็ไม่ออกมาเจอ","หัวหัวหอมอยากได้น้องเพิ่มไหมจ๊ะ"];
                foreach($str as $s){
 		 echo "<label><div class ='bubble'><div class='bubble-content'> ".$s."</div> </div></label>";
